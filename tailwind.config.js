22568422242c279796347a56084495f536525a23
Raúl López <hi@raullg.com>
2022-07-11 21:24:10 +0100

    wip: report mockup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -13,6 +14,10 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                primary: colors.pink,
+                secondary: colors.purple,
+            }
         },
     },
 
