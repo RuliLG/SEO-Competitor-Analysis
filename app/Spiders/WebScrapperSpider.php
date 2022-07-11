096f7836330c7b4ffceb0b1f8bd9763ed8c32fa5
IsmaelSantiagoR <santiago@calimasolutions.com>
2022-07-11 21:01:40 +0100

    + get html and text of an url

---
+++ app/Spiders/WebScrapperSpider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Spiders;
+
+use Generator;
+use Html2Text\Html2Text;
+use RoachPHP\Downloader\Middleware\RequestDeduplicationMiddleware;
+use RoachPHP\Extensions\LoggerExtension;
+use RoachPHP\Extensions\StatsCollectorExtension;
+use RoachPHP\Http\Response;
+use RoachPHP\Spider\BasicSpider;
+use RoachPHP\Spider\ParseResult;
+
+class WebScrapperSpider extends BasicSpider
+{
+    public array $startUrls = [
+        'https://raullg.com/'
+    ];
+
+    public array $downloaderMiddleware = [
+        RequestDeduplicationMiddleware::class,
+    ];
+
+    public array $spiderMiddleware = [
+        //
+    ];
+
+    public array $itemProcessors = [
+        //
+    ];
+
+    public array $extensions = [
+        LoggerExtension::class,
+        StatsCollectorExtension::class,
+    ];
+
+    public int $concurrency = 2;
+
+    public int $requestDelay = 1;
+
+    /**
+     * @return Generator<ParseResult>
+     */
+    public function parse(Response $response): Generator
+    {
+        $bodyHTML = new Html2Text($response->filter('body')->html());
+        $plainText = $bodyHTML->getText();
+    }
+}
